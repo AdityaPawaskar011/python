4b9070877e7ead4ea5c521d5f1f25096a5836361
AdityaPawaskar011 <adityapawaskar40@gmail.com>
2025-04-29 11:10:11 +0530

    practiced for loop

---
+++ test.py
@@ -0,0 +1,14 @@
+a = [2,6,7,8,3,8,9,3,34]
+
+def getInded(a):
+    print("hello")
+    for x in range(0, len(a)):
+       if a[x] %2 ==0:
+        print(x,a[x])
+       else: 
+        pass     
+
+
+
+
+getInded(a)
